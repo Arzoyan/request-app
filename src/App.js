ddd5928ce8ddb96ea0aa6328d970aca23a7e330e
Ruben Arzoyan <ruben.arzoyan@gmail.com>
2019-12-10 09:27:54 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ModalForm from "./components/ModalForm"
+import TableForm from "./components/TableForm"
+
+
+const App = () => {
+  return (
+    <div>
+      uuhuh
+{
+        // <ModalForm />
+        <TableForm />
+
+      }
+      <div>
+        999
+</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/helpers/utils/constants.js
@@ -0,0 +1,134 @@
+export const USERS = [
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Տոնսեր",
+        service: ""
+    },
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Տոնսեր",
+        service: "Տոնսեր"
+    },
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Արտագնա",
+        service: ""
+    },
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Տոնսեր",
+        service: "Տոնսեր"
+    },
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Տոնսեր",
+        service: ""
+    },
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Տոնսեր",
+        service: ""
+    },
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Տոնսեր",
+        service: ""
+    },
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Տոնսեր",
+        service: ""
+    },
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Տոնսեր",
+        service: ""
+    },
+    {
+        name: "Գագիկ",
+        lastName: "Մագիկյան",
+        phoneNumber: "+37491112113",
+        type: "Տոնսեր",
+        service: ""
+    }
+];
+
+export const TYPE = [
+    "Տոնսեր",
+    "Տոնսեր",
+    "Արտագնա",
+    "Տոնսեր",
+    "Տոնսեր",
+    "Տոնսեր",
+    "Տոնսեր",
+    "Տոնսեր",
+    "Տոնսեր",
+    "Տոնսեր",
+    "Տոնսեր"
+];
+export const SERVICE = [
+    "",
+    "",
+    "Արտագնա",
+    "Տոնսեր",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "Տոնսեր",
+    ""
+];
+export const TIME = [
+    1,
+    162,
+    239,
+    311,
+    289,
+    11,
+    50,
+    111,
+    123,
+    68
+];
+export const STAGE = [
+    {
+        type: "Registration",
+        shape: "circle",
+        number: "0.4"
+    },
+    {
+        type: "Processing",
+        shape: "treangle",
+        number: "1.9"
+    },
+    {
+        type: "Performing",
+        shape: "square",
+        number: "2.3"
+    },
+    {
+        type: "conclusion",
+        shape: "Pentagon",
+        number: "3.3"
+    }
+];
+
